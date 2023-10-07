1cfc1aba33edc9137660860822f4bba57be9bb87
VishnuIsHere <129365187+VishnuIsHere@users.noreply.github.com>
2023-10-07 17:50:40 +0530

    Add files via upload

---
+++ vproject/Myproject/app1/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Games(models.Model):
+    title = models.CharField(max_length=255)
+    pdf = models.FileField(upload_to='pages/')
+    cover = models.FileField(upload_to='pic/')
